a88d68c377482d91e13ee3921feb18af0bb3b5d2
Jingwei "John" Liu <liujingwei@gmail.com>
2016-10-08 13:34:06 +0800

    Compactibility update with Swift 3

--- scres.swift
+++ scres.swift
@@ -15,18 +15,22 @@ import ApplicationServices
 import CoreVideo
 
 
+
 // Swift String is quite powerless at the moment
 // http://stackoverflow.com/questions/24044851
 // http://openradar.appspot.com/radar?id=6373877630369792
 extension String {
     func substringFromLastOcurrenceOf(needle:String) -> String {
-        var str = self
-        while let range = str.rangeOfString(needle) {
-            let index2 = range.startIndex.advancedBy(1)
-            let range2 = Range<String.Index>(index2..<str.endIndex)
-            str = str.substringWithRange(range2)
+        var substring = String.init()
+        for (_, c) in self.characters.enumerated() {
+            substring.append(c)
+            
+            if (c == "/") {
+                substring.removeAll()
+            }
         }
-        return str
+
+        return substring
     }
 
     func toUInt() -> UInt? {
@@ -43,12 +47,12 @@ var maxDisplays:UInt32 = 8
 var displayCount32:UInt32 = 0
 
 // store displayID in an raw array
-var onlineDisplayIDs = UnsafeMutablePointer<CGDirectDisplayID>.alloc(Int(maxDisplays))
+var onlineDisplayIDs = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity:Int(maxDisplays))
 
 func main () -> Void {
     let error:CGError = CGGetOnlineDisplayList(maxDisplays, onlineDisplayIDs, &displayCount32)
     
-    if (error != .Success) {
+    if (error != .success) {
 		print("Error on getting online display List.");
         return
     }
@@ -57,7 +61,7 @@ func main () -> Void {
 
 
     // strip path and leave filename
-    let binary_name = Process.arguments[0].substringFromLastOcurrenceOf("/")
+    let binary_name = CommandLine.arguments[0].substringFromLastOcurrenceOf(needle:"/")
 
     // help message
     let help_msg = ([
@@ -72,27 +76,27 @@ func main () -> Void {
                 "   -l          list displays\n",
                 "   -m 0        list all mode from a certain display\n",
                 "   -s 0 800    set resolution of display 0 to 800*600\n",
-                ]).joinWithSeparator("")
+                ]).joined(separator:"")
     let help_display_list = "List all available displays by:\n    \(binary_name) -l"
     
 
-    let argc = Process.arguments.count
+    let argc = CommandLine.arguments.count
     if argc > 1 {
-        if Process.arguments[1] == "-l" || Process.arguments[1] == "--list" {
-            listDisplays(onlineDisplayIDs, count:displayCount)
+        if CommandLine.arguments[1] == "-l" || CommandLine.arguments[1] == "--list" {
+            listDisplays(displayIDs:onlineDisplayIDs, count:displayCount)
             return
         }
 
-        if Process.arguments[1] == "-m" || Process.arguments[1] == "--mode" {
+        if CommandLine.arguments[1] == "-m" || CommandLine.arguments[1] == "--mode" {
             if argc < 3 {
                 print("Specify a display to see its supported modes. \(help_display_list)")
                 return
             }
 
-            if let displayIndex = Int(Process.arguments[2]) {
+            if let displayIndex = Int(CommandLine.arguments[2]) {
                 if displayIndex < displayCount {
-                    let _info = listModesByDisplayID(onlineDisplayIDs[displayIndex]) 
-                    displayModes(onlineDisplayIDs[displayIndex], index:displayIndex, _modes:_info)
+                    let _info = listModesByDisplayID(_displayID:onlineDisplayIDs[displayIndex])
+                    displayModes(_display:onlineDisplayIDs[displayIndex], index:displayIndex, _modes:_info)
                     
                 } else {
                     print("Display index: \(displayIndex) not found. \(help_display_list)")
@@ -105,34 +109,34 @@ func main () -> Void {
 
         
         
-        if Process.arguments[1] == "-s" || Process.arguments[1] == "--set" {
+        if CommandLine.arguments[1] == "-s" || CommandLine.arguments[1] == "--set" {
             if argc < 3 {
                 print("Specify a display to set its mode. \(help_display_list)")
                 return
             }
 
             var singleDisplayMode = false
-            if argc < 4 {
-                if Int(Process.arguments[2]) < 10 {
-                    print("Specify display width")
-                    return
-                } else {
-                    if (displayCount == 1) {
-                        singleDisplayMode = true
-                    } else {
-                        print("Specify display index")
-                        return
-                    }
-                }
-            }
 
             
-            if  var displayIndex = Int(Process.arguments[2]) {
+            if  var displayIndex = Int(CommandLine.arguments[2]) {
+                if argc < 4 {
+                    if displayIndex < 10 {
+                        print("Specify display width")
+                        return
+                    } else {
+                        if (displayCount == 1) {
+                            singleDisplayMode = true
+                        } else {
+                            print("Specify display index")
+                            return
+                        }
+                    }
+                }
                 let designatedWidthOptional: UInt?
                 
                 if singleDisplayMode == true {
                     displayIndex = 0
-                    designatedWidthOptional = Process.arguments[2].toUInt()
+                    designatedWidthOptional = CommandLine.arguments[2].toUInt()
                     
                 }
                 
@@ -142,22 +146,23 @@ func main () -> Void {
                         return
                     }
                     
-                    designatedWidthOptional = Process.arguments[3].toUInt()
+                    designatedWidthOptional = CommandLine.arguments[3].toUInt()
                 }
                 
                 if let designatedWidth = designatedWidthOptional {
-                    if let modesArray = listModesByDisplayID(onlineDisplayIDs[displayIndex]) {
+                    if let modesArray = listModesByDisplayID(_displayID:onlineDisplayIDs[displayIndex]) {
                         var designatedWidthIndex:Int?
                         for i in 0..<modesArray.count {
-                            let di = displayInfo(onlineDisplayIDs[displayIndex], mode:modesArray[i])
-                            if di.width == designatedWidth {
-                                designatedWidthIndex = i
-                                break
+                            if let di = displayInfo(display:onlineDisplayIDs[displayIndex], mode:modesArray[i]) {
+                                if di.width == designatedWidth {
+                                    designatedWidthIndex = i
+                                    break
+                                }
                             }
                         }
                         if designatedWidthIndex != nil {
                             print("setting display mode")
-                            setDisplayMode(onlineDisplayIDs[displayIndex], mode:modesArray[designatedWidthIndex!], designatedWidth:designatedWidth)
+                            setDisplayMode(display:onlineDisplayIDs[displayIndex], mode:modesArray[designatedWidthIndex!], designatedWidth:designatedWidth)
                         }
                         else {
                             print("This mode is unavailable for current desktop GUI")
@@ -176,21 +181,17 @@ func main () -> Void {
 }
 
 func setDisplayMode(display:CGDirectDisplayID, mode:CGDisplayMode, designatedWidth:UInt) -> Void {
-    if CGDisplayModeIsUsableForDesktopGUI(mode) {
+    if mode.isUsableForDesktopGUI() {
     
-        let config = UnsafeMutablePointer<CGDisplayConfigRef>.alloc(1);
+        let config = UnsafeMutablePointer<CGDisplayConfigRef?>.allocate(capacity:1);
     
         let error = CGBeginDisplayConfiguration(config)
-        if error == .Success {
-            if nil != config {
-                let option:CGConfigureOption = CGConfigureOption(rawValue:2) //XXX: permanently
-                CGConfigureDisplayWithDisplayMode(config.memory, display, mode, nil)
-                let afterCheck = CGCompleteDisplayConfiguration(config.memory, option)
-                if afterCheck != .Success {
-                    CGCancelDisplayConfiguration(config.memory)
-                }
-            } else {
-                print("Setting display mode failed")
+        if error == .success {
+            let option:CGConfigureOption = CGConfigureOption(rawValue:2) //XXX: permanently
+            CGConfigureDisplayWithDisplayMode(config.pointee, display, mode, nil)
+            let afterCheck = CGCompleteDisplayConfiguration(config.pointee, option)
+            if afterCheck != .success {
+                CGCancelDisplayConfiguration(config.pointee)
             }
         }
     } else {
@@ -208,7 +209,7 @@ func listModesByDisplayID(_displayID:CGDirectDisplayID?) -> [CGDisplayMode]? {
             for i in 0..<count {
                 let modeRaw = CFArrayGetValueAtIndex(modeList, i)
                 // https://github.com/FUKUZAWA-Tadashi/FHCCommander
-                let mode = unsafeBitCast(modeRaw, CGDisplayMode.self)
+                let mode = unsafeBitCast(modeRaw, to:CGDisplayMode.self)
 
                 modesArray.append(mode)
             }
@@ -224,14 +225,15 @@ func displayModes(_display:CGDirectDisplayID?, index:Int, _modes:[CGDisplayMode]
         if let modes = _modes {
             print("Supported Modes for Display \(index):")
             
-            let nf = NSNumberFormatter()
-            nf.paddingPosition = NSNumberFormatterPadPosition.BeforePrefix
+            let nf = NumberFormatter()
+            nf.paddingPosition = NumberFormatter.PadPosition.beforePrefix
             nf.paddingCharacter = " " // XXX: Swift does not support padding yet
             nf.minimumIntegerDigits = 3 // XXX
 
             for i in 0..<modes.count {
-                let di = displayInfo(display, mode:modes[i])
-                print("       \(nf.stringFromNumber(di.width)!) * \(nf.stringFromNumber(di.height)!) @ \(di.frequency)Hz")
+                if let di = displayInfo(display:display, mode:modes[i]) {
+                    print("       \(di.width) * \(di.height) @ \(di.frequency)Hz")
+                }
             }
         }
     }
@@ -241,8 +243,9 @@ func displayModes(_display:CGDirectDisplayID?, index:Int, _modes:[CGDisplayMode]
 // used by -l
 func listDisplays(displayIDs:UnsafeMutablePointer<CGDirectDisplayID>, count:Int) -> Void {
     for i in 0..<count {
-        let di = displayInfo(displayIDs[i], mode:nil)
-        print("Display \(i):  \(di.width) * \(di.height) @ \(di.frequency)Hz")
+        if let di = displayInfo(display:displayIDs[i], mode:nil) {
+            print("Display \(i):  \(di.width) * \(di.height) @ \(di.frequency)Hz")
+        }
     }
 }
 
@@ -251,33 +254,36 @@ struct DisplayInfo {
     var width:UInt, height:UInt, frequency:UInt
 }
 // return with, height and frequency info for corresponding displayID
-func displayInfo(display:CGDirectDisplayID, mode:CGDisplayMode?) -> DisplayInfo {
+func displayInfo(display:CGDirectDisplayID, mode:CGDisplayMode?) -> DisplayInfo? {
     var mode_ = mode
     if mode_ == nil {
         mode_ = CGDisplayCopyDisplayMode(display)!
     }
     
-    let width = UInt( CGDisplayModeGetWidth(mode) )
-    let height = UInt( CGDisplayModeGetHeight(mode) )
-    var frequency = UInt( CGDisplayModeGetRefreshRate(mode) )
-
-    if frequency == 0 {
-        var link:CVDisplayLink?
+    if let mode__ = mode_ {
+        let width = UInt( mode__.width )
+        let height = UInt( mode__.height )
+        var frequency = UInt( mode__.refreshRate )
         
-        CVDisplayLinkCreateWithCGDisplay(display, &link)
-    
-        let time:CVTime = CVDisplayLinkGetNominalOutputVideoRefreshPeriod(link!)
-
-        // timeValue is in fact already in Int64
-        let timeValue = time.timeValue as Int64
-
-        // a hack-y way to do ceil
-        let timeScale = Int64(time.timeScale) + timeValue / 2
-
-        frequency = UInt( timeScale / timeValue )
+        if frequency == 0 {
+            var link:CVDisplayLink?
+            
+            CVDisplayLinkCreateWithCGDisplay(display, &link)
+            
+            let time:CVTime = CVDisplayLinkGetNominalOutputVideoRefreshPeriod(link!)
+            
+            // timeValue is in fact already in Int64
+            let timeValue = time.timeValue as Int64
+            
+            // a hack-y way to do ceil
+            let timeScale = Int64(time.timeScale) + timeValue / 2
+            
+            frequency = UInt( timeScale / timeValue )
+        }
+        
+        return DisplayInfo(width:width, height:height, frequency:frequency)
     }
-
-    return DisplayInfo(width:width, height:height, frequency:frequency)
+    return nil
 }
 
 // run it
