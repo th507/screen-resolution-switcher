0b1dc9c8d36479f8d37849c736660319fcc2b9b0
Lucas Garron <code@garron.net>
2018-06-13 02:02:43 -0700

    Add scale to README documentation.

--- scres.swift
+++ scres.swift
@@ -32,7 +32,7 @@ func main () -> Void {
         "usage: ",
         "\(binary_name) ",
         "[-h|--help] [-l|--list|list] [-m|--mode|mode displayIndex] \n",
-        "[-s|--set|set displayIndex width]",
+        "[-s|--set|set displayIndex width scale]",
         "\n\n",
         
         "Here are some examples:\n",
